5831718676a85eadde47164a7e2f0c8cf130ab04
JuanAntonioMN <tonynepo2001@gmail.com>
2025-01-29 01:08:10 -0600

    dat

--- src/script/fetchBooks.js
+++ src/script/fetchBooks.js
@@ -1,4 +1,4 @@
-export const fetchBooks = async () => {
+/*export const fetchBooks = async () => {
     try {
       const response = await fetch("https://moneyhoneyb.onrender.com/libros"); // Asegúrate de que esta URL es la correcta para tu API
       const data = await response.json();
@@ -16,3 +16,77 @@ export const fetchBooks = async () => {
     }
   };
   
+*/
+
+
+
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    const response = await fetch("https://moneyhoneyb.onrender.com/libros");
+    const data = await response.json();
+
+    if (response.ok) {
+      const libro = data.libros;
+      const container = document.getElementById("container-libros");
+
+      // Limpiamos el contenedor antes de agregar nuevos libros
+      container.innerHTML = '';
+
+      libro.forEach((libros) => {
+        const libroElement = document.createElement('div');
+        libroElement.className="flex w-full lg:flex-row  md:flex-col flex-col justify-center items-center sm:items-start px-5 sm:p-0 md:p-0 lg:px-10 gap-5";
+        
+
+        let texto="";
+          if(libros.estado){
+            texto="Poner en venta"
+          }else{
+                texto="Poner libro gratis"
+            }
+        // Creamos el contenido de cada libro
+        libroElement.innerHTML = `
+         <img class="w-full  md:w-full lg:w-[25%]" src="https://moneyhoneyb.onrender.com/uploads/images/books/${libros.imagen_url}" alt=${libros.nombre} id="imagen_url" />
+        <div class="w-full flex flex-col lg:px-5 lg:gap-32 md:gap-5 gap-5">
+            <article class="flex flex-col w-full sm:gap-5 lg:order-1 md:order-2 order-2">
+                <h1 class="font-opensans font-semibold text-[1.5rem]" id="nombre">${libros.nombre}</h1>
+                <h2 class="font-opensans font-semibold text-[1.3rem]">Reseña del libro</h2>
+                <p class="text-[1.2rem]" id="descripcion">${libros.descripcion}</p>
+                <p class="font-opensans font-semibold text-[1.1rem]" id="texto-${libros.id}">Precio del libro: <b class="font-extralight">${libros.precio}</b></p>
+                <p class="font-opensans font-semibold text-[1.1rem]" id="mostrar-${libros.id}">Cupón: <b class="font-extralight">cupon</b></p>
+    
+            </article>
+            <div class="w-full flex  gap-5 flex-wrap lg:order-2 md:order-1 order-1">
+                <button id="modificar-${libros.id}" class="w-full lg:w-[20%] md:w-full bg-[#8C52FF] font-opensans text-white text-[1rem] rounded-[2rem] h-12 p-2 shadow-color hover:bg-[#D7B9FF] hover:text-[#8C52FF] transition-all ease-out delay-150 duration-150">
+                    <i class="fa-solid fa-pen-to-square"></i> Modificar libro
+                </button>
+
+             <button  id="cambiar-${libros.id}" data-id=${libros.estado}   class="w-full lg:w-[20%] md:w-full hover:bg-[#8C52FF] bg-black font-opensans text-white text-[1rem] rounded-[2rem] h-12 p-2 hover:shadow-color transition-all ease-out delay-150 duration-150">
+                   ${texto}
+                </button>
+
+
+                <button  id="cupon-${libros.id}"  class="w-full lg:w-[20%] md:w-full bg-black font-opensans text-white text-[1rem] rounded-[2rem] h-12 p-2 hover:shadow-color transition-all ease-out delay-150 duration-150">
+                    <i class="fa-solid fa-receipt"></i> Generar cupón
+                </button>
+
+                <button id="eliminar-${libros.id}" class="w-full lg:w-[20%] md:w-full bg-black font-opensans text-white text-[1rem] rounded-[2rem] h-12 p-2 hover:shadow-color hover:bg-[#D7B9FF] hover:text-black transition-all ease-out delay-150 duration-150">
+                    <i class="fa-solid fa-trash"></i> Eliminar libro
+                </button>
+            </div>
+        </div
+        `;
+
+        // Agregamos el libro al contenedor
+        container.appendChild(libroElement);
+      });
+    } else {
+      console.error('Error al obtener los libros:', data.mensaje || 'Error desconocido');
+    }
+  } catch (error) {
+    console.error('Error al obtener los libros:', error);
+  }
+})
+  
+        
+    
+   
--- src/script/fetchUsuarios.js
+++ src/script/fetchUsuarios.js
@@ -1,4 +1,4 @@
-export const fetchUsuarios = async () => {
+/*export const fetchUsuarios = async () => {
     try {
       const response = await fetch("https://moneyhoneyb.onrender.com/usuarios"); // Asegúrate de que esta URL es la correcta para tu API
       const data = await response.json();
@@ -14,4 +14,94 @@ export const fetchUsuarios = async () => {
       return [];
     }
   };
-  
+  */
+
+  document.addEventListener("DOMContentLoaded", async () => {
+    try {
+      const response = await fetch("https://moneyhoneyb.onrender.com/usuarios");
+      const data = await response.json();
+
+      if (response.ok) {
+        const usuario = data.usuarios;
+        const container = document.getElementById("container-usuarios");
+
+        // Limpiamos el contenedor antes de agregar nuevos libros
+        container.innerHTML = '';
+
+        usuario.forEach((usuario) => {
+          const usuarioElement = document.createElement('div');
+          usuarioElement.className="bg-white  shadow rounded-lg w-full sm:w-full md:w-5/6  lg:w-1/2 xl:w-1/3 mx-auto";
+          
+          // Creamos el contenido de cada libro
+          usuarioElement.innerHTML = `
+           <div class="flex justify-center">
+                    
+                        <img src="https://moneyhoneyb.onrender.com/uploads/images/users/${usuario.url_img}"  alt=${usuario.nombre} class="rounded-full mx-auto  w-32 h-32 shadow-md border-4 border-white transition duration-200 transform hover:scale-110">
+                </div>
+                
+                <div class="mt-2">
+                    <h1 class="font-bold text-center text-3xl text-gray-900">${usuario.nombre}</h1>
+                    <p class="text-center text-sm text-gray-400 font-medium">${usuario.resultado}</p>
+                    
+                    <div class="my-5 px-6 w-full">
+                        <button id="eliminar-${usuario.id}" class="text-gray-200  w-full rounded-lg text-center font-medium leading-6 px-6 py-3 bg-[#8C52FF] hover:bg-black hover:text-white">Eliminar usuario</button>
+                    </div>
+                   
+
+                    <div class="w-full">
+                        <h3 class="font-medium text-gray-900 text-left px-6">Información del usuario</h3>
+                        <div class="mt-5 w-full flex flex-col items-center overflow-hidden text-sm">
+                            <a href="#" class=" border-t border-gray-100 text-gray-600 py-4 pl-6 pr-3 w-full block hover:bg-gray-100 transition duration-150 overflow-hidden">
+                                <img src="https://avatars0.githubusercontent.com/u/35900628?v=4" alt="" class="rounded-full h-6 shadow-md inline-block mr-2">
+                                Institución
+                                <span class="text-gray-500 text-xs">${usuario.institucion}</span>
+                            </a>
+                            
+                            <a href="#" class=" border-t border-gray-100 text-gray-600 py-4 pl-6 pr-3 w-full block hover:bg-gray-100 transition duration-150">
+                                <img src="https://avatars0.githubusercontent.com/u/35900628?v=4" alt="" class="rounded-full h-6 shadow-md inline-block mr-2">
+                                   Nombre
+                                    <span class="text-gray-500 text-xs">${usuario.nombre}</span>
+                            </a>
+                            <a href="#" class=" border-t border-gray-100 text-gray-600 py-4 pl-6 pr-3 w-full block hover:bg-gray-100 transition duration-150">
+                                <img src="https://avatars0.githubusercontent.com/u/35900628?v=4" alt="" class="rounded-full h-6 shadow-md inline-block mr-2">
+                                Numero de telefono
+                                <span class="text-gray-500 text-xs">${usuario.telefono}</span>
+                            </a>
+
+                            <a href="#" class=" border-t border-gray-100 text-gray-600 py-4 pl-6 pr-3 w-full block hover:bg-gray-100 transition duration-150">
+                                <img src="https://avatars0.githubusercontent.com/u/35900628?v=4" alt="" class="rounded-full h-6 shadow-md inline-block mr-2">
+                               correo
+                                <span class="text-gray-500 text-xs">${usuario.correo}</span>
+                            </a>
+
+                            <a href="#" class=" border-t border-gray-100 text-gray-600 py-4 pl-6 pr-3 w-full block hover:bg-gray-100 transition duration-150">
+                                <img src="https://avatars0.githubusercontent.com/u/35900628?v=4" alt="" class="rounded-full h-6 shadow-md inline-block mr-2">
+                                    Edad
+                                    <span class="text-gray-500 text-xs">${usuario.edad}</span>
+                            </a>
+
+                            <a href="#" class=" border-t border-gray-100 text-gray-600 py-4 pl-6 pr-3 w-full block hover:bg-gray-100 transition duration-150">
+                                <img src="https://avatars0.githubusercontent.com/u/35900628?v=4" alt="" class="rounded-full h-6 shadow-md inline-block mr-2">
+                               Sexo 
+                                <span class="text-gray-500 text-xs">${usuario.sexo}</span>
+                            </a>
+
+                           
+
+                           
+                            
+                        </div>
+                    </div>
+                </div>
+          `;
+
+          // Agregamos el libro al contenedor
+          container.appendChild(usuarioElement);
+        });
+      } else {
+        console.error('Error al obtener los libros:', data.mensaje || 'Error desconocido');
+      }
+    } catch (error) {
+      console.error('Error al obtener los libros:', error);
+    }
+  })  
